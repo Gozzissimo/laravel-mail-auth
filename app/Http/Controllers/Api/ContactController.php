45c40be73a5a2bf363d65a1a80fdef16f2a9984b
Gozzissimo <fantini.nico@gmail.com>
2022-03-14 19:04:17 +0100

    Form Validator

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
     public function store (Request $request) 
     {
         $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required',
+        ]);
+
+        if ($validator->fails) {
+            return response()->json([
+                'success'=> false,
+                'errors'=> $validator->errors()
+            ]);
+        }
     }
 }
